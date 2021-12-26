3982b50eff2eab3d561637d1f5988c1a6e760c48
Neha <nehaprajapati1150@gmail.com>
2021-12-26 14:50:03 +0530

    searchfield state

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -7,7 +7,8 @@ class App extends Component{
   constructor(){
     super();
     this.state={
-      monsters:[]
+      monsters:[],
+      searchField:''
     }
   }
   componentDidMount(){
@@ -20,6 +21,9 @@ class App extends Component{
  render() { 
    return (
     <div className="App">
+      <input type='search' placeholder='Search Monster' onChange={e=>this.setState({searchField:e.target.value},()=>{     //second arguement is a function call to make state syncronus,as setState is asynchronus and gives a empty value in search value at first time
+        console.log(this.state);
+      })}/>
       <CardList monsters={this.state.monsters}/>
      
     </div>
