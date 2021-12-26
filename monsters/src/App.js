c2649047f872cff9999ae45d01474049c6ffb45a
Neha <nehaprajapati1150@gmail.com>
2021-12-26 12:26:01 +0530

    fetch

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -6,23 +6,20 @@ class App extends Component{
   constructor(){
     super();
     this.state={
-      monster:[
-        {
-          name:"Dracula"
-        },{
-          name:"Dinasour"
-        },{
-          name:"Zombie"
-        }
-      ]
+      monster:[]
     }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({monster:users}))
   }
 
 
  render() { 
    return (
     <div className="App">
-     {this.state.monster.map(monster=> <h1>{monster.name}</h1>)}
+     {this.state.monster.map(monster=> <h1 key={monster.id}>{monster.name}</h1>)}
     </div>
   );
  }
