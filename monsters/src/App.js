e4408f321b12fd53e5b460214d031731a01669ae
Neha <nehaprajapati1150@gmail.com>
2021-12-26 15:23:47 +0530

    searchbox component

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,7 +1,7 @@
 import {Component} from 'react';
 import './App.css';
 import { CardList } from './components/CardList/CardListComponent';
-
+import { SearchBox } from './components/searchBox/SearchboxComponent';
 
 class App extends Component{
   constructor(){
@@ -23,9 +23,7 @@ class App extends Component{
    const filteredMonsters=monsters.filter(monster=>monster.name.toLowerCase().includes(searchField.toLowerCase()))
    return (
     <div className="App">
-      <input type='search' placeholder='Search Monster' onChange={e=>this.setState({searchField:e.target.value},()=>{     //second arguement is a function call to make state syncronus,as setState is asynchronus and gives a empty value in search value at first time
-        console.log(this.state);
-      })}/>
+      <SearchBox placeholder='Search Monster' handleChange={e=>this.setState({searchField:e.target.value})}/>
       <CardList monsters={filteredMonsters}/>
      
     </div>
